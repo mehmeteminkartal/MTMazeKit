7128abc5e155d33f5a92187f16ed19c9f1483762
Mehmet Emin KARTAL <kartalbombe@icloud.com>
2017-12-19 21:45:42 +0300

    Initial Commit

---
+++ MTMazeKit/MTMazeKit.h
@@ -0,0 +1,19 @@
+//
+//  MTMazeKit.h
+//  MTMazeKit
+//
+//  Created by Muhammet Mehmet Emin Kartal on 12/19/17.
+//  Copyright © 2017 Mekatrotekno. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MTMazeKit.
+FOUNDATION_EXPORT double MTMazeKitVersionNumber;
+
+//! Project version string for MTMazeKit.
+FOUNDATION_EXPORT const unsigned char MTMazeKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MTMazeKit/PublicHeader.h>
+
+
